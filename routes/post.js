ccbfd4a7f33bbfc35c5f6d111aa43a94d5774f6b
albin <albinyewen@gmail.com>
2016-01-29 16:39:23 +0800

    add hooks

--- routes/post.js
+++ routes/post.js
@@ -1,10 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
+var exec = require('child_process').exec;
 
 /* GET home page. */
 router.post('/', function(req, res) {
-    console.log(req);
-    res.send(200, "Success!");
+    var commits = req.body.commits;
+    var git_path = path.resolve('.');
+    exec("cd " + git_path + "&& git pull origin master", function(err, stdout, stderr) {
+        if(err) {
+            res.send(err);
+        }else {
+            commits.added.forEach(function(file) {
+                db.add(file);
+            });
+
+            commits.removed.forEach(function(){
+                db.remove(file);
+            });
+
+            commits.modified.forEach(function(){
+                db.mod(file);
+            });
+            res.send(200);
+        }
+    })
 });
 
 module.exports = router;
